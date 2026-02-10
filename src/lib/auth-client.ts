b95b4a98d8b9d5e243402df224439001b3134334
unknown <mohammadshihabctg@gmail.com>
2026-02-11 00:08:10 +0600

    Error handling in production

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { env } from "@/env";
 import { createAuthClient } from "better-auth/react";
 
-const NEXT_PUBLIC_AUTH_URL = env.NEXT_PUBLIC_API_URL;
+const NEXT_PUBLIC_AUTH_URL = env.NEXT_PUBLIC_AUTH_URL;
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
   baseURL: `${NEXT_PUBLIC_AUTH_URL!}`,
