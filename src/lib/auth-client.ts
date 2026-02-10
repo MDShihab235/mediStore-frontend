df7e61e9e043f2f43fd5a39130f1aed59df7074d
unknown <mohammadshihabctg@gmail.com>
2026-02-10 23:57:16 +0600

    Error handling in production

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,17 +1,20 @@
-// import { env } from "@/env";
-// import { createAuthClient } from "better-auth/react";
-
-// const NEXT_PUBLIC_API_URL = env.NEXT_PUBLIC_API_URL;
-// export const authClient = createAuthClient({
-//   /** The base URL of the server (optional if you're using the same domain) */
-//   baseURL: `${NEXT_PUBLIC_API_URL!}`,
-// });
-
+import { env } from "@/env";
 import { createAuthClient } from "better-auth/react";
 
+const NEXT_PUBLIC_API_URL = env.NEXT_PUBLIC_API_URL;
 export const authClient = createAuthClient({
-  baseURL: typeof window !== "undefined" ? window.location.origin : "",
+  /** The base URL of the server (optional if you're using the same domain) */
+  baseURL: `${NEXT_PUBLIC_API_URL!}`,
   fetchOptions: {
     credentials: "include",
   },
 });
+
+// import { createAuthClient } from "better-auth/react";
+
+// export const authClient = createAuthClient({
+//   baseURL: typeof window !== "undefined" ? window.location.origin : "",
+//   fetchOptions: {
+//     credentials: "include",
+//   },
+// });
